e25b95876a3cfa739c4d5b271e6d66cfacf4252f
streetlightkids <ltj_hq@hotmail.com>
2022-03-24 15:07:13 -0700

    transport App JS and CSS from wrecked repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Head from "./components/Head";
+import Nav from "./components/Nav";
+import Button from "./components/Button";
+import Appointments from "./components/Appointments";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head />
+      <Nav />
+      <div className="appts">
+        <h1>My Appointments</h1>
+        <div className="apptsTypeCont">
+          <Button>Upcoming Appointments</Button>
+          <Button>Past Appointments</Button>
+        </div>
+        <Appointments />
+      </div>
     </div>
   );
 }
-
-export default App;
